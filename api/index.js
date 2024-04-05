f9c3fa4c77cc50eb1daeffa6b4bd4ccb30396a67
Sofi Crisafulli <71233317+SofiaCrisafulli@users.noreply.github.com>
2024-04-05 14:27:06 -0300

    Anda en local

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const cors = require('cors');
+
+app.use(cors());
+
 app.use(express.json()); // Para parsear el cuerpo de las solicitudes JSON
 
 app.get("/datos", (req, res) => res.send("proyecto-react/src/datos.json"));
