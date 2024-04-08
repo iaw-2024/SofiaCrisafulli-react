c3243567a3c5f5a20c45bac0247177f1a98d61d5
Sofi Crisafulli <71233317+SofiaCrisafulli@users.noreply.github.com>
2024-04-08 16:12:45 -0300

    fghjk

--- proyecto-react/src/App.js
+++ proyecto-react/src/App.js
@@ -32,27 +32,4 @@ function App() {
   );
 }
 
-/*function App() {
-  return (
-    <div className="App">
-      <table className="App-table centered-table">
-        <thead>
-          <tr>
-            <th>Email</th>
-            <th>Género</th>
-          </tr>
-        </thead>
-        <tbody>
-          {datos.map((usuario) => (
-            <tr key={usuario.email}>
-              <td>{usuario.email}</td>
-              <td>{usuario.gender}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-}*/
-
 export default App;
